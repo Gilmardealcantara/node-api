efa9302eb715012925f5c49774038e646191af9a
Gilmar de Alcantara <gilmar.alcantara@ibm.com>
2019-02-16 16:12:05 -0200

    create middleware for validate token

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth')
 
 const router = express.Router();
 
+router.use(authMiddleware);
 
 router.get('/', (req, res) => {
+    // eslint-disable-next-line no-console
+    console.log(req.userId)
     res.send({ ok: true})
 });
 
